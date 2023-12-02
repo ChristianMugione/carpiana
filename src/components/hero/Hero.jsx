149b4875c3d31e67c09362c67bb3882338541dd4
Christian Mugione <129909700+ChristianMugione@users.noreply.github.com>
2023-12-02 20:03:57 +0000

    Update 16 files

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import "./HeroStyles.css";
+
+export const Hero = () => {
+  return (
+    <section className="section">
+      <h2>CREAMOS TU PRESENCIA ONLINE</h2>
+      <h3>Desarrollamos sitios web que cautivan y convierten</h3>
+      <button>TRABAJEMOS JUNTOS</button>
+      <button>MAS INFO</button>
+    </section>
+  );
+};
